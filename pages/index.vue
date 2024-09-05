13951a17aec2126e34bf8c8e3cdc45700cd58688
kekke36 <64622253+kekke36@users.noreply.github.com>
2024-09-05 13:22:28 +0000

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+type Test = {
+  name: string;
+  checked: boolean;
+};
+
+const test: Test = {
+  name: "test",
+  checked: true,
+};
+</script>
+
+<template>index</template>
