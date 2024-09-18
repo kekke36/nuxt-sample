290b64c346cacfe309495cf9886e8099a4f3fb69
kekke36 <64622253+kekke36@users.noreply.github.com>
2024-09-18 16:33:18 +0000

    msw導入

---
+++ mocks/handlers.ts
@@ -0,0 +1,13 @@
+import { http, HttpResponse } from "msw";
+
+export const handlers = [
+  http.get("http://localhost:3000/api/books", () => {
+    return HttpResponse.json({
+      books: [
+        { id: 1, title: "雨ニモマケズ", author: "宮沢 賢治" },
+        { id: 2, title: "走れメロス", author: "太宰 治" },
+        { id: 3, title: "こころ", author: "夏目 漱石" },
+      ],
+    });
+  }),
+];
--- pages/api-test.vue
+++ pages/api-test.vue
@@ -1,1 +1,16 @@
-<template></template>
+<script setup lang="ts">
+const { data: books } = await useFetch<{
+  books: { id: number; title: string; author: string }[];
+}>("http://localhost:3000/api/books");
+</script>
+
+<template>
+  <main>
+    <h1>Books</h1>
+    <ul>
+      <li v-for="{ id, title, author } of books?.books" :key="id">
+        {{ title }} ({{ author }})
+      </li>
+    </ul>
+  </main>
+</template>
