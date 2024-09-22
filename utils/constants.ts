c6540e477ebcb4ba66f7e25f0509a4cba8fcd8d6
kekke36 <64622253+kekke36@users.noreply.github.com>
2024-09-22 08:16:39 +0000

    add constants

---
+++ utils/constants.ts
@@ -0,0 +1,4 @@
+import { asdf } from "./constants/asdf";
+import { test } from "./constants/test";
+
+export const constants = { test, asdf } as const;
---
+++ utils/constants/asdf.ts
@@ -0,0 +1,3 @@
+export const asdf = {
+  ASDF: "asdf",
+};
