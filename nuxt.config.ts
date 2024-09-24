81ef8611385eeb76d41a4eed1d0dd435fecd6c38
kekke36 <64622253+kekke36@users.noreply.github.com>
2024-09-24 15:10:05 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,24 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: false, // NOTE: SSRだと初期表示時にMSWのモックAPIが呼べない
-  modules: ["@nuxt/eslint", "@hebilicious/vue-query-nuxt"],
+  modules: ["@nuxt/eslint", "@hebilicious/vue-query-nuxt", "nuxt-icon"],
+  typescript: {
+    typeCheck: true,
+  },
+  vueQuery: {
+    vueQueryPluginOptions: {
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: true,
+          },
+        },
+      },
+    },
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.NUXT_BASE_URL,
+    },
+  },
 });
---
+++ pages/icon.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Icon</h1>
+    <Icon name="uil:github" color="black" />
+  </div>
+</template>
